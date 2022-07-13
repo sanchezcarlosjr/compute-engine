b53600d47a871d0bbf836d3d017f9384024161f6
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <hello@sanchezcarlosjr.com>
2022-07-13 14:00:45 -0500

    ignore binary files

--- calculator.cpp
+++
@@ -1,9 +0,0 @@
-#include "nanolibc/libc.h"
-#include "nanolibc/libc_extra.h"
-
-#define WASM_EXPORT __attribute__((visibility("default"))) extern "C"
- 
-WASM_EXPORT int calculate() {
-	return 0;
-}
-
